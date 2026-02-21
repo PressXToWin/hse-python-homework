0d57c1f36334fdbe8681b7c0ff843f9bc948a2ca
PressXToWin <75699872+PressXToWin@users.noreply.github.com>
2026-02-21 13:14:07 +0700

    Добавление графика обращений к DNS

--- 11/main.py
+++ 11/main.py
@@ -8,6 +8,7 @@ with open('botsv1.json', 'r', encoding='utf-8') as file:
 
 data_rows = [item['result'] for item in data if 'result' in item]
 df = pd.DataFrame(data_rows)
+dns_df = df.query("app == 'dns'")
 
 def main_graph(dataframe):
     sns.countplot(data=dataframe, y="EventCode")
@@ -16,7 +17,16 @@ def main_graph(dataframe):
     plt.ylabel("EventCode")
     plt.show()
 
+def dns_graph(dataframe):
+    sns.countplot(data=dataframe, y="QueryName")
+    plt.title("Обращение к доменным именам")
+    plt.xlabel("Количество обращений")
+    plt.ylabel("Domain name")
+    plt.show()
+
 
 if __name__ == '__main__':
     print(df.head())
     main_graph(df)
+    print(dns_df.head())
+    dns_graph(dns_df)
